03ea3f993d33018bdcf980060cd4b79f7545dbeb
Roland Pheasant <roland_pheasant@hotmail.com>
2016-07-03 19:58:45 +0100

    Start filter example

---
+++ SelectableItems/Examples/FilterViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using DynamicData.Binding;
+
+namespace DynamicData.Samplz.Examples
+{
+    public class FilterViewModel : AbstractNotifyPropertyChanged
+    {
+        private string _searchText;
+
+        public FilterViewModel()
+        {
+            var  trades = new StockTicker("EUR",1.31, DateTime.Now);
+        }
+
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set { SetAndRaise(ref _searchText, value); }
+        }
+
+        private IObservable<StockTicker> TradesGenerator()
+        {
+            //var generator =  0<Trade>()
+            //bit of code to generate trades
+            var random = new Random();
+
+            return Observable.Empty<StockTicker>();
+
+        } 
+    } 
+
+    public class StockTicker
+    {
+        public DateTime DateRecieved { get; }
+        public double Price { get; }
+        public string Symbol { get; }
+
+        public StockTicker(string symbol, double price, DateTime dateRecieved)
+        {
+            Symbol = symbol;
+            Price = price;
+            DateRecieved = dateRecieved;
+        }
+    }
+
+    public static class Ex
+    {
+        public static IEnumerable<int> Primes(int bound)
+        {
+            if (bound < 2) yield break;
+            //The first prime number is 2
+            yield return 2;
+
+            BitArray composite = new BitArray((bound - 1) / 2);
+            int limit = ((int)(Math.Sqrt(bound)) - 1) / 2;
+            for (int i = 0; i < limit; i++)
+            {
+                if (composite[i]) continue;
+                //The first number not crossed out is prime
+                int prime = 2 * i + 3;
+                yield return prime;
+                //cross out all multiples of this prime, starting at the prime squared
+                for (int j = (prime * prime - 2) >> 1; j < composite.Count; j += prime)
+                {
+                    composite[j] = true;
+                }
+            }
+            //The remaining numbers not crossed out are also prime
+            for (int i = limit; i < composite.Count; i++)
+            {
+                if (!composite[i]) yield return 2 * i + 3;
+            }
+        }
+    }
+}
--- SelectableItems/Infrastructure/SampleItemCollection.cs
+++ SelectableItems/Infrastructure/SampleItemCollection.cs
@@ -11,13 +11,18 @@ namespace DynamicData.Samplz.Infrastructure
         {
             Items = new List<SampleItem>
             {
+               //new SampleItem("Filtering", new FilterViewModel(), 
+               //         "Filter on user entered text",
+               //         "FilterViewModel.cs",
+               //         "https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/SelectableItems/Examples/FilterViewModel.cs"),
+
                 new SampleItem("Selectable Items", new SelectableItemsViewModel(),
                         "Filter on an object which implements INotifyPropertyChanged",
                         "SelectableItemsViewModel.cs",
                         "https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/SelectableItems/Examples/SelectableItemsViewModel.cs"),
 
                 new SampleItem("Aggregations", new AggregationViewModel(),
-                        "Aggregate over a collection filtered on a property"
+                        "Aggregate over a collection which is filtered on a property"
                         ,"AggregationViewModel.cs"
                         ,"https://github.com/RolandPheasant/DynamicData.Samplz/blob/master/SelectableItems/Examples/AggregationViewModel.cs")
             };
